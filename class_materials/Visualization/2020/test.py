bbc63c0d560d8153cb0cec73e07411e8c6043281
Ben Heil <ben.jer.heil@gmail.com>
2020-06-15 11:43:11 -0400

    Added question 2

--- class_materials/Visualization/2020/test.py
+++ class_materials/Visualization/2020/test.py
@@ -29,3 +29,46 @@ def test_regression(x, y, prediction_slope, prediction_intercept):
         print('Your calculate_regression_coeff function has not been implemented yet')
         return
     print('Your calculate_regression_coeff function is correct')
+
+def test_heatmap_read(df):
+    '''Test that the student read in the dataframe correctly'''
+    try:
+        assert len(df) == 30
+    except AssertionError:
+        print("Your dataframe isn't the correct length. Are you sure you read the right file?")
+        return
+    except TypeError:
+        print('Your create_dataframe function has not been implemented yet')
+        return
+    try:
+        assert len(df.columns) == 6
+    except AssertionError:
+        print("Your dataframe doesn't have the correct number of columns, are you sure you read the right file?")
+        return
+
+    print('You successfully read in your dataframe')
+
+def test_heatmap_melt(df):
+    '''Test that the heatmap dataframe was melted correctly'''
+    try:
+        assert len(df.columns) == 3
+    except AssertionError:
+        print("Your dataframe doesn't have the correct number of columns.")
+        print("The dataframe should have a column for genes, case/control status, and expression")
+        print("Printing the first few rows of your dataframe with df.head() might help find the bug")
+        return
+    except AttributeError:
+        print('Your melt_dataframe function has not been implemented yet')
+        return
+
+    try:
+        assert len(df) == 150
+    except AssertionError:
+        print("Your dataframe doessn't have the correct number of rows.")
+        print("The dataframe should have a column for genes, case/control status, and expression")
+        print("Printing the first few rows of your dataframe with df.head() might help find the bug")
+        return
+
+    print('You successfully reformated your dataframe')
+
+
